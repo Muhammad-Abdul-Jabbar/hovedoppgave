acd5e82417f636a9fb005d50617a70230c50b693
Abdul Jabbar <122173146+Muhammad-Abdul-Jabbar@users.noreply.github.com>
2024-04-11 11:17:54 +0200

    css,html.js

---
+++ JavaScript/firebase-config.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBUxSe6guEi8HsQ1mTmx5Cka41R6LXVUKw",
+    authDomain: "tentamen-pr.firebaseapp.com",
+    projectId: "tentamen-pr",
+    storageBucket: "tentamen-pr.appspot.com",
+    messagingSenderId: "506204527398",
+    appId: "1:506204527398:web:cbefb3fd40aad41bcfffe7"
+  };
