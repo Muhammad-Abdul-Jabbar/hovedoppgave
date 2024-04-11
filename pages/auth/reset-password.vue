a5880d68e08001605ed591328e6db14ac99ba9a1
Torkild Dyvik Olsen <torkild@tdolsen.net>
2024-04-11 12:39:58 +0200

    init: nuxt sacffolding

---
+++ pages/auth/reset-password.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+const email = ref("");
+
+async function resetPassword() {
+	const result = await $fetch("/api/auth/reset-password", {
+		method: "POST",
+		headers: {
+			"Content-Type": "application/json",
+		},
+		body: {
+			email: email.value,
+		},
+	});
+
+	console.log(result);
+}
+</script>
+
+<template>
+	<div>
+		<h1>Reset password</h1>
+		<p>
+			<input v-model="email" type="email" placeholder="Email" />
+		</p>
+		<p>
+			<button @click="resetPassword">Reset password</button>
+		</p>
+	</div>
+</template>
